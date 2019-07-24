d8689c4ca7436ff4cc648c21c79634c12d63c427
Mat Anderson <matandersonlv@hotmail.com>
2019-07-24 23:35:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+<<<<<<< HEAD
     @user = User.find_by(username: params[:username])
     if @user != nil && @user.password == params[:password]
+=======
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+>>>>>>> 4e55f31f65d8aac44dfb56463ea4672e0ba911b6
       session[:user_id] = @user.id
       redirect '/account'
     end
@@ -20,12 +25,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+<<<<<<< HEAD
     @current_user = User.find_by_id(session[:user_id])
     if @current_user
       erb :account
     else
       erb :error
     end
+=======
+    
+>>>>>>> 4e55f31f65d8aac44dfb56463ea4672e0ba911b6
   end
 
   get '/logout' do
